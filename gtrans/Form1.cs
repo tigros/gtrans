5b24f74e2fe23f01fa8749c72db63612d0af764a
tigros <straightupandup@yahoo.com>
2019-01-01 10:43:49 -0500

    improve performance

--- gtrans/Form1.cs
+++ gtrans/Form1.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.Win32;
 using System;
 using System.Collections;
+using System.Drawing;
 using System.IO;
 using System.Text;
 using System.Threading;
@@ -71,6 +72,33 @@ namespace gtrans
             return a;
         }
 
+        private void newbrow()
+        {
+            this.SuspendLayout();
+            textBox1.SuspendLayout();
+            textBox1.Enabled = false;
+            Size savesize = new Size(webBrowser1.Size.Width, webBrowser1.Size.Height); 
+            webBrowser1.DocumentCompleted -= new WebBrowserDocumentCompletedEventHandler(webBrowser1_DocumentCompleted);
+            this.Controls.Remove(this.webBrowser1);
+            webBrowser1.Dispose();
+            webBrowser1 = null;
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.webBrowser1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.webBrowser1.Location = new System.Drawing.Point(1048, 25);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = savesize;
+            this.webBrowser1.TabIndex = 2;
+            this.Controls.Add(this.webBrowser1);
+            webBrowser1.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(webBrowser1_DocumentCompleted);
+            webBrowser1.ScriptErrorsSuppressed = true;
+            textBox1.Enabled = true;
+            textBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
 
         private void button1_Click(object sender, EventArgs e)
         {
@@ -141,6 +169,9 @@ namespace gtrans
                     {
                         textBox2.AppendText(" " + sb.ToString());
                         sb.Clear();
+                        if (count % 100 == 0)
+                            newbrow();
+                        GC.Collect();
                     }
                 }
                 
